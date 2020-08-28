dea5beb332c4fc74f3088af7b6a3e04a44be5996
Leonardo <leosud099@gmail.com>
2020-08-27 18:09:39 -0600

    Updated projects with find the image game

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -16,48 +16,44 @@ const projectsData = {
   projects: [
     {
       id: 1,
-      name: "LeoNoronhaSantos",
+      name: "Leo Noronha Santos",
       url: "https://github.com/leonoronhas/portfolio",
-      img: "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/website.gif",
-      technologies: [
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "React.js",
-        "Redux",
-      ],
+      img:
+        "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/website.gif",
+      technologies: ["HTML", "CSS", "JavaScript", "React.js", "Redux"],
     },
     {
       id: 2,
-      name: "FoodApp",
+      name: "Food App",
       url: "https://www.github.com/leonoronhas/foodApp",
-      img: "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/foodApp.gif",
-      technologies: [
-        "JSX",
-        "React Native",
-        "JavaScript",
-        "React.js",
-        "Redux",
-      ],
+      img:
+        "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/foodApp.gif",
+      technologies: ["JSX", "React Native", "JavaScript", "React.js", "Redux"],
     },
     {
       id: 3,
-      name: "GuessNumberGame",
-      url: "https://www.github.com/leonoronhas/GuessNumberGame",
-      img: "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/guessApp.gif",
-      technologies: [
-        "JSX",
-        "React Native",
-        "JavaScript",
-        "React.js",
-      ],
+      name: "RESTful API tutorial",
+      url:
+        "https://www.linkedin.com/pulse/create-restful-api-nodejs-expressjs-mongodb-leonardo-noronha-santos/",
+      img:
+        "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/restfulapi.png",
+      technologies: ["MongoDB", "Express", "Node", "REST"],
     },
     {
       id: 4,
-      name: "RESTful API tutorial",
-      url: "https://www.linkedin.com/pulse/create-restful-api-nodejs-expressjs-mongodb-leonardo-noronha-santos/",
-      img: "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/restfulapi.png",
-      technologies: ["MongoDB", "Express", "Node", "REST"],
+      name: "Guess Number Game",
+      url: "https://www.github.com/leonoronhas/GuessNumberGame",
+      img:
+        "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/guessApp.gif",
+      technologies: ["JSX", "React Native", "JavaScript", "React.js"],
+    },
+    {
+      id: 5,
+      name: "Find the Image!",
+      url: "https://www.github.com/leonoronhas/find-the-image",
+      img:
+        "https://raw.githubusercontent.com/leonoronhas/portfolio/master/src/images/guessApp.gif",
+      technologies: ["JSX", "React Native", "JavaScript", "Expo"],
     },
   ],
 };
